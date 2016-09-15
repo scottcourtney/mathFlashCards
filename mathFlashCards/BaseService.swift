f7c7b1cf3aed821183686a3f6d8920ab9e2adac6
Scott Courtney <scott.courtney@qonceptual.com>
2016-09-14 22:34:39 -0400

    UI and some logic

---
+++ mathFlashCards/BaseService.swift
@@ -0,0 +1,9 @@
+//
+//  BaseService.swift
+//  mathFlashCards
+//
+//  Created by Scott Courtney on 9/14/16.
+//  Copyright © 2016 Scott Courtney. All rights reserved.
+//
+
+import Foundation
---
+++ mathFlashCards/LoginController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginController.swift
+//  mathFlashCards
+//
+//  Created by Scott Courtney on 9/13/16.
+//  Copyright © 2016 Scott Courtney. All rights reserved.
+//
+
+import Foundation
